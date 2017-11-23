16130368a47ba8ffcb6de00279337a1a24c6381a
AudricPandele <audric.pandele@ingesup.com>
2017-11-23 16:49:42 +0100

    Interests

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -22,6 +22,36 @@ async function getUserWithSportList(req, res) {
 	res.ok(user)
 }
 
+async function getUserInterests(req, res) {
+	const user = (await User.findOne({ id: req.params.id })
+		.populate('events'))
+	.toJSON()
+
+	user.sportList = await SportList.find({ user: user.id })
+		.populate('sport')
+		.populate('level')
+
+	let tmp = []
+	for (var i = 0; i < user.sportList.length; i++) {
+		let event = await Event.find({ sport: user.sportList[i].sport.id, level: user.sportList[i].level.id })
+		tmp.push(event)
+	}
+
+	let events = []
+	events = [].concat.apply([], tmp);
+
+	let final = []
+	for (var i = 0; i < events.length; i++) {
+		let event = (await Event.findOne({ id: events[i].id}).populate('sport').populate('level').populate('owner')).toJSON()
+		event.participants = await Group.find({event : event.id}).populate('user').populate('status')
+		final.push(event)
+	}
+
+	res.ok(final)
+
+}
+
 module.exports = {
-	show: (req, res, next) => { getUserWithSportList(req, res).catch(next) }
+	show: (req, res, next) => { getUserWithSportList(req, res).catch(next) },
+	interests: (req, res, next) => { getUserInterests(req, res).catch(next) }
 };
--- config/routes.js
+++ config/routes.js
@@ -42,6 +42,7 @@ module.exports.routes = {
   'get /user/:id' : 'UserController.show',
   'get /event/:id' : 'EventController.show',
   'get /opinion/:id' : 'OpinionController.show',
+  'get /user/:id/interests' : 'UserController.interests',
 
   /***************************************************************************
   *                                                                          *
