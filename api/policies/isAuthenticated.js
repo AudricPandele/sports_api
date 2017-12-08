d74039efbd777f48fbf77813ea85319669d25173
AudricPandele <audric.pandele@ingesup.com>
2017-12-08 11:38:26 +0100

    Policies

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -1,11 +1,14 @@
 var passport = require('passport');
 
 module.exports = function (req, res, next) {
-  passport.authenticate('jwt', function(error,user,info){
-    if(error) return res.serverError(error);
+  passport.authenticate('jwt', function(err,user,info){
+    if(err) return res.serverError(err);
       if(!user)
-        return res.unauthorized(null,info)
+        return res.json( { message: info.message })
       req.user = user;
       next()
+    // if (err) { return next(err) }
+    // if (!user) { return res.json( { message: info.message }) }
+    // res.json(user);
   }) (req, res)
 }
